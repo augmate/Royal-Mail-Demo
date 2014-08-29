7fdc37b7e211c90372aa6ed15194da202ad3cc33
Prem Nirmal <prem@fuzzproductions.com>
2014-08-29 17:51:39 -0400

    added brightness scale

--- augmate-sample/src/main/java/com/augmate/sample/BrightnessActivity.java
+++ augmate-sample/src/main/java/com/augmate/sample/BrightnessActivity.java
@@ -7,6 +7,7 @@ import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.augmate.sample.common.SoundHelper;
 import com.google.android.glass.touchpad.GestureDetector;
@@ -18,39 +19,41 @@ public class BrightnessActivity extends Activity {
 
     private int brightness;
     private GestureDetector mGestureDetector;
-    private final GestureDetector.ScrollListener mBaseListener = new GestureDetector.ScrollListener() {
+
+    private final class Listener implements GestureDetector.ScrollListener {
 
         @Override
         public boolean onScroll(float displacement, float delta, float velocity) {
             if (displacement < 0) {
-                brightness -= 10;
+                brightness -= 3;
             } else if (displacement > 0) {
-                brightness += 10;
+                brightness += 3;
             }
             setBrightness();
             return true;
         }
-    };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mGestureDetector = new GestureDetector(this).setScrollListener(mBaseListener);
+        mGestureDetector = new GestureDetector(this).setScrollListener(new Listener());
         setContentView(R.layout.slider);
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
         seekBar.setMax(100);
-        float set_brightness = getSharedPreferences(getApplication().getPackageName(),MODE_PRIVATE).getFloat("BRIGHTNESS",0.5f);
+        float set_brightness = getSharedPreferences(getApplication().getPackageName(), MODE_PRIVATE).getFloat("BRIGHTNESS", 0.5f);
         brightness = (int) (set_brightness * 100f);
         setBrightness();
     }
 
     private void setBrightness() {
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
+        TextView brightnessText = (TextView) findViewById(R.id.brightness_value);
         Window w = getWindow();
         WindowManager.LayoutParams lp = w.getAttributes();
         lp.screenBrightness = (float) brightness / 100f;
-        if (lp.screenBrightness < .01f) {
-            brightness = 1;
+        if (lp.screenBrightness <= .01f) {
+            brightness = 0;
         } else if (lp.screenBrightness > 1f) {
             brightness = 100;
         }
@@ -59,6 +62,8 @@ public class BrightnessActivity extends Activity {
                 .putFloat("BRIGHTNESS", lp.screenBrightness).apply();
         w.setAttributes(lp);
         seekBar.setProgress(brightness);
+        String brightnessString = (brightness * 230 / 100 + 25) + ""; // make the scale from 25 to 255
+        brightnessText.setText(brightnessString);
     }
 
     @Override
@@ -71,6 +76,7 @@ public class BrightnessActivity extends Activity {
         if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
             SoundHelper.tap(this);
             finish();
+            return true;
         }
         return super.onKeyDown(keyCode, event);
     }
