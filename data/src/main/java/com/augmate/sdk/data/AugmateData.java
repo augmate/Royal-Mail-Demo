d3a4a2b5ae5b86a10d8b6b6bea4709dd15771a0b
Johny Urgiles <johny@augmate.com>
2014-09-03 18:04:46 -0400

    Update make data sdk somewhat more usable, need to move TestDataActivity into an actual test

--- data/src/main/java/com/augmate/sdk/data/AugmateData.java
+++ data/src/main/java/com/augmate/sdk/data/AugmateData.java
@@ -1,11 +1,15 @@
 package com.augmate.sdk.data;
 
 import android.content.Context;
+import com.google.gson.Gson;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class AugmateData {
 
+    public static final String PARSE_KEY_PAYLOAD = "payload";
     private static boolean initialized = false;
 
     public AugmateData(Context context) {
@@ -16,12 +20,23 @@ public class AugmateData {
                 AugmateData.initialized = true;
             }
         }
-
     }
 
-    public void test() {
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
+    public void save(String className, Object obj) {
+        ParseObject testObject = new ParseObject(className);
+        testObject.put(PARSE_KEY_PAYLOAD, new Gson().toJson(obj));
         testObject.saveInBackground();
+    }
+
+    public String read(String className) {
+        ParseObject object = null;
+
+        try {
+            object = new ParseQuery<>(className).orderByDescending("updatedAt").getFirst();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return object.getString(PARSE_KEY_PAYLOAD);
     }
 }
--- data/src/main/java/com/augmate/sdk/data/TestDataActivity.java
+++ data/src/main/java/com/augmate/sdk/data/TestDataActivity.java
@@ -2,6 +2,9 @@ package com.augmate.sdk.data;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class TestDataActivity extends Activity {
 
@@ -10,6 +13,28 @@ public class TestDataActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AugmateData(this).test();
+        SampleAugmateData obj1 = new SampleAugmateData("obj1 in array", 999);
+        SampleAugmateData obj2 = new SampleAugmateData("obj2 in array", 99);
+
+        ArrayList<SampleAugmateData> sampleAugmateDatas = new ArrayList<>();
+        sampleAugmateDatas.add(obj1);
+        sampleAugmateDatas.add(obj2);
+
+        String expected = new Gson().toJson(sampleAugmateDatas);
+
+        new AugmateData(this).save("ArrayList", sampleAugmateDatas);
+        String readData = new AugmateData(this).read("ArrayList");
+
+        assert(expected.equals(readData));
+    }
+
+    static private class SampleAugmateData {
+        public SampleAugmateData(String str, int integer) {
+            this.str = str;
+            this.integer = integer;
+        }
+
+        String str;
+        int integer;
     }
 }
