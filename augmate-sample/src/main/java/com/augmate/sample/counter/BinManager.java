166d3b8341f45803946cde3204bb955c4d2ff9f5
Johny Urgiles <johny@augmate.com>
2014-09-03 19:31:32 -0400

    In parallel to existing local bin storage, upload the data to parse

--- augmate-sample/src/main/java/com/augmate/sample/counter/BinManager.java
+++ augmate-sample/src/main/java/com/augmate/sample/counter/BinManager.java
@@ -1,8 +1,10 @@
 package com.augmate.sample.counter;
 
 import android.content.Context;
-
+import android.util.Log;
 import com.augmate.sample.AugmateApplication;
+import com.augmate.sdk.data.AugmateData;
+import com.google.gson.Gson;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -16,8 +18,10 @@ import java.util.ArrayList;
 public class BinManager {
 
     public static final String TAG = "BinManager";
+    public static final String BINS_CLASS_NAME = "CycleCountingBins";
 
     private static BinManager sharedManager;
+    private static AugmateData augmateData;
     ArrayList<BinModel> mBins;
 
 
@@ -34,6 +38,7 @@ public class BinManager {
     }
 
     private BinManager() {
+        augmateData = new AugmateData(AugmateApplication.getContext());
         read();
     }
 
@@ -42,16 +47,26 @@ public class BinManager {
             FileOutputStream out = AugmateApplication.getContext().openFileOutput(TAG, Context.MODE_PRIVATE);
             ObjectOutputStream objectOut = new ObjectOutputStream(out);
             objectOut.writeObject(mBins);
+
+            augmateData.save(BINS_CLASS_NAME, mBins);
+
         } catch (Throwable t) {
         }
     }
 
     private void read() {
         try {
+            Log.d(getClass().getName(), "Attempting to read local store");
+
             FileInputStream out = AugmateApplication.getContext().openFileInput(TAG);
             ObjectInputStream objectOut = new ObjectInputStream(out);
             mBins = (ArrayList<BinModel>) objectOut.readObject();
+
+            String binJson = augmateData.read(BINS_CLASS_NAME);
+            ArrayList<BinModel> arrayList = new Gson().fromJson(binJson, ArrayList.class);
+
         } catch (Throwable ignored) {
+            Log.d(getClass().getName(), "Reading failed");
             mBins = new ArrayList<>();
             save();
         }
