316af80ac1d181d819d7bccb8302b9076e77e571
Alex Barkan <alex@augmate.com>
2014-09-04 16:34:39 -0400

    improved beacon sdk info

--- beacon-test/src/main/java/com/augmate/beacontest/app/MainActivity.java
+++ beacon-test/src/main/java/com/augmate/beacontest/app/MainActivity.java
@@ -4,12 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.WindowManager;
 import android.widget.TextView;
 import com.augmate.sdk.beacons.BeaconDistance;
 import com.augmate.sdk.beacons.BeaconInfo;
 import com.augmate.sdk.logger.Log;
 
-import java.util.Map;
+import java.util.Collection;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,32 +20,9 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        beaconDistance.configureFromContext(this);
-        beaconDistance.startListening();
-
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                Map<Integer, BeaconInfo> beaconDistances = beaconDistance.getLatestBeaconDistances();
-
-                String displayMsg = String.format("%d beacons are within the area\n", beaconDistances.size());
-
-                Log.debug("We know about %d beacons within the area", beaconDistances.size());
-                for (BeaconInfo beacon : beaconDistances.values()) {
-                    Log.debug("  beacon %s %d is %.2f units away", beacon.beaconName, beacon.uniqueId, beacon.distance);
-
-                    displayMsg += String.format("%s %d is %.2f units away\n", beacon.beaconName, beacon.uniqueId, beacon.distance);
-                }
-
-                Message msg = displayMsgUpdater.obtainMessage();
-                Bundle bundle = new Bundle();
-                bundle.putString("msg", displayMsg);
-                msg.setData(bundle);
-                msg.sendToTarget();
-            }
-        }, 0, 2000);
     }
 
     private Handler displayMsgUpdater = new Handler() {
@@ -56,9 +34,42 @@ public class MainActivity extends Activity {
     };
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
+        Log.debug("Pausing..");
+
         timer.cancel();
         beaconDistance.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.debug("Resuming..");
+
+        beaconDistance.configureFromContext(this);
+        beaconDistance.startListening();
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Collection<BeaconInfo> beaconDistances = beaconDistance.getLatestBeaconDistances();
+
+                String displayMsg = String.format("%d beacons are within the area\n", beaconDistances.size());
+
+                //Log.debug("We know about %d beacons within the area", beaconDistances.size());
+                for (BeaconInfo beacon : beaconDistances) {
+                    //Log.debug("  beacon %s %d is %.2f units away", beacon.beaconName, beacon.uniqueId, beacon.distance);
+                    displayMsg += String.format("[%s] dist=%.2f skew=%.2f percentile=%.2f\n", beacon.uniqueId, beacon.distanceMean, beacon.distanceSkewness, beacon.distancePercentile);
+                }
+
+                Message msg = displayMsgUpdater.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putString("msg", displayMsg);
+                msg.setData(bundle);
+                msg.sendToTarget();
+            }
+        }, 0, 2000);
     }
 }
