ecfb9fd5b9e469f2df3b976fb82acc69338a6fd0
Johny Urgiles <johny@augmate.com>
2014-09-05 00:45:52 -0400

    skip downloading android dependencies if already done

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,18 @@
-echo y | android update sdk -a  --no-ui --filter android-19
-echo y | android update sdk -a  --no-ui --filter tools
-echo y | android update sdk -a  --no-ui --filter platform-tools 
-echo y | android update sdk -a  --no-ui --filter build-tools-20.0.0
-echo y | android update sdk -a  --no-ui --filter extra-android-m2repository
-echo y | android update sdk -a  --no-ui --filter extra-android-support
-echo y | android update sdk -a  --no-ui --filter extra-google-m2repository
-echo y | android update sdk -a  --no-ui --filter addon-google_gdk-google-19
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed_dependencies"
+
+if [ ! -e $DEPS ]; then
+  echo y | android update sdk -a  --no-ui --filter android-19
+  echo y | android update sdk -a  --no-ui --filter tools
+  echo y | android update sdk -a  --no-ui --filter platform-tools 
+  echo y | android update sdk -a  --no-ui --filter build-tools-20.0.0
+  echo y | android update sdk -a  --no-ui --filter extra-android-m2repository
+  echo y | android update sdk -a  --no-ui --filter extra-android-support
+  echo y | android update sdk -a  --no-ui --filter extra-google-m2repository
+  echo y | android update sdk -a  --no-ui --filter addon-google_gdk-google-19
+  touch $DEPS
+fi
