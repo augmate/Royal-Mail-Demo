cfa3fab8b3a5c5a0c95e04e92826238e1e030c1e
Johny Urgiles <johny@augmate.com>
2014-09-05 00:28:11 -0400

    add another support dependency to fix circleci build

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,1 @@
-echo y | android update sdk -a  --no-ui --filter android-19,tools,platform-tools,build-tools-20.0.0,extra-android-support,extra-google-m2repository,addon-google_gdk-google-19
+echo y | android update sdk -a  --no-ui --filter android-19,tools,platform-tools,build-tools-20.0.0,extra-android-m2repository,extra-android-support,extra-google-m2repository,addon-google_gdk-google-19
