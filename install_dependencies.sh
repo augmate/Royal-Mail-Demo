23e54fc1d308b450e885c69df5fcdf9932fe1728
Johny Urgiles <johny@augmate.com>
2014-09-05 00:36:40 -0400

    expand android dependencies to fix accepting licenses

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,8 @@
-echo y | android update sdk -a  --no-ui --filter android-19,tools,platform-tools,build-tools-20.0.0,extra-android-m2repository,extra-android-support,extra-google-m2repository,addon-google_gdk-google-19
+echo y | android update sdk -a  --no-ui --filter android-19
+echo y | android update sdk -a  --no-ui --filter tools
+echo y | android update sdk -a  --no-ui --filter platform-tools 
+echo y | android update sdk -a  --no-ui --filter build-tools-20.0.0
+echo y | android update sdk -a  --no-ui --filter extra-android-m2repository
+echo y | android update sdk -a  --no-ui --filter extra-android-support
+echo y | android update sdk -a  --no-ui --filter extra-google-m2repository
+echo y | android update sdk -a  --no-ui --filter addon-google_gdk-google-19
