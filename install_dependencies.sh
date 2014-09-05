3990cea591dd018c52f9e4d7d4c7f7b35890b949
Johny Urgiles <johny@augmate.com>
2014-09-05 01:14:18 -0400

    install ndk and build artifact in circleci

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,7 @@
 
 # Fix the CircleCI path
 export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+export PATH="$NDK_HOME/bin:$PATH"
 
 DEPS="$ANDROID_HOME/installed_dependencies"
 
@@ -14,5 +15,10 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -a  --no-ui --filter extra-android-support
   echo y | android update sdk -a  --no-ui --filter extra-google-m2repository
   echo y | android update sdk -a  --no-ui --filter addon-google_gdk-google-19
+  
+  # Install NDK
+  wget https://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 > /dev/null;
+  tar -jxf --directory /usr/local android-ndk32-r10-linux-x86_64.tar.bz2
+
   touch $DEPS
 fi
