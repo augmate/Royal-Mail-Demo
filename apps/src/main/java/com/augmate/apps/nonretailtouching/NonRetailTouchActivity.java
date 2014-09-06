e91d29acde979e9d9a5ff9d01aa6f69130ce14b6
Alex Barkan <alex@augmate.com>
2014-09-06 18:06:24 -0400

    NRT added unique barcode counter

--- apps/src/main/java/com/augmate/apps/nonretailtouching/NonRetailTouchActivity.java
+++ apps/src/main/java/com/augmate/apps/nonretailtouching/NonRetailTouchActivity.java
@@ -2,12 +2,17 @@ package com.augmate.apps.nonretailtouching;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 import com.augmate.apps.R;
 import com.augmate.apps.common.activities.BaseActivity;
 import com.augmate.sdk.logger.Log;
 import com.augmate.sdk.scanner.ScannerFragmentBase;
 
+import java.util.ArrayList;
+
 public class NonRetailTouchActivity extends BaseActivity implements ScannerFragmentBase.OnScannerResultListener {
+
+    private ArrayList<String> recordedBarcodes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,16 @@ public class NonRetailTouchActivity extends BaseActivity implements ScannerFragm
     @Override
     public void onBarcodeScanSuccess(String result) {
         Log.debug("Got scanning result: [%s]", result);
+
+        if(!recordedBarcodes.contains(result)) {
+            recordedBarcodes.add(result);
+
+            Log.debug("-> unique scanning result: [%s] (array size: %d)", result, recordedBarcodes.size());
+
+            // we have a new unique barcode
+            TextView nrtCounter = (TextView) findViewById(R.id.nrt_counter);
+            nrtCounter.setText("" + recordedBarcodes.size());
+            nrtCounter.setTextColor(0xFFFFFF00);
+        }
     }
 }
