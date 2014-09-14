ad59d848d7d803aaabd9ac1c1023ccf97a099674
Alex Barkan <alex@augmate.com>
2014-09-13 22:53:40 -0400

    added bluetooth scanner test activity and updated to new sdk

--- experiments/beacon-test/src/main/java/com/augmate/beacontest/app/BluetoothPairingActivity.java
+++ experiments/beacon-test/src/main/java/com/augmate/beacontest/app/BluetoothPairingActivity.java
@@ -1,106 +1,114 @@
 package com.augmate.beacontest.app;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.*;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
 import android.view.WindowManager;
+import android.widget.TextView;
 import com.augmate.sdk.logger.Log;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.Timer;
+import com.augmate.sdk.scanner.bluetooth.BluetoothBarcodeScannerService;
 
 public class BluetoothPairingActivity extends Activity {
-    private Timer timer = new Timer();
-    private BluetoothAdapter bluetoothAdapter;
-    private BluetoothBroadcastReceiver mReceiver;
+    private BluetoothBarcodeScannerService bluetoothScannerService;
+    private ServiceConnection bluetoothScannerConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            bluetoothScannerService = ((BluetoothBarcodeScannerService.ScannerBinder) service).getService();
+            Log.debug("Service connected: " + name.flattenToShortString());
+        }
 
-    private class BluetoothBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.debug("Service disconnected: " + name.flattenToShortString());
+            bluetoothScannerService = null;
+        }
+    };
+    /**
+     * Receive simple messages from BluetoothBarcodeScannerService
+     */
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction(); //may need to chain this to a recognizing function
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            String action = intent.getAction();
 
-                Log.debug("Found device: [%s] @ [%s]", device.getName(), device.getAddress());
+            switch (action) {
+                case BluetoothBarcodeScannerService.ACTION_BARCODE_SCANNED:
+                    String barcode = intent.getStringExtra(BluetoothBarcodeScannerService.EXTRA_BARCODE_STRING);
+                    Log.debug("Received barcode value: [%s]", barcode);
+                    ((TextView) findViewById(R.id.barcodeScannerResults)).append(String.format("\nScanned barcode: [%s]", barcode));
+                    break;
 
-                if (
-                        "00:1C:97:90:8A:4F".equals(device.getAddress()) // ecom handheld barcode scanner
-                        || "F8:A9:D0:AC:51:77".equals(device.getAddress()) // alex's phone
-                    ) {
-                    Log.debug("device.getBondState= " + device.getBondState());
-                    Log.debug("device.getBluetoothClass = " + device.getBluetoothClass());
-                    Log.debug("device.getType = " + device.getType());
-                    Log.debug("device.getName = " + device.getName());
+                case BluetoothBarcodeScannerService.ACTION_SCANNER_CONNECTED:
+                    ((TextView) findViewById(R.id.barcodeScannerStatus)).setText("Scanner Connected");
+                    ((TextView) findViewById(R.id.barcodeScannerStatus)).setTextColor(0xFF00FF00);
+                    ((TextView) findViewById(R.id.barcodeScannerResults)).append("\nScanner Connected");
+                    break;
 
-                    try {
-                        Log.debug("attempting to unpair device..");
-                        device.getClass().getMethod("removeBond", (Class[]) null).invoke(device, (Object[]) null);
-                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                        Log.exception(e, "Error unpairing device!");
-                    }
+                case BluetoothBarcodeScannerService.ACTION_SCANNER_DISCONNECTED:
+                    ((TextView) findViewById(R.id.barcodeScannerStatus)).setText("No Scanner");
+                    ((TextView) findViewById(R.id.barcodeScannerStatus)).setTextColor(0xFFFF0000);
+                    ((TextView) findViewById(R.id.barcodeScannerResults)).append("\nScanner Disconnected");
+                    break;
 
-                    Log.debug("starting pairing..");
-                    boolean bCreateBond = device.createBond();
-                    Log.debug("bCreateBond = " + bCreateBond);
-                }
-            }
-
-            if (BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)) {
-                int prevBondState = intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, -1);
-                int bondState = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1);
-                Log.debug("previous bond state=" + prevBondState);
-                Log.debug("current bond state=" + bondState);
-
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Log.debug("got device = " + device);
-
-                if (bondState == BluetoothDevice.BOND_BONDING) {
-                    Log.debug("confirming pairing.."); // not sure if this is necessary
-                    device.setPairingConfirmation(true);
-                }
+                default:
+                    Log.warn("Unhandled intent action: %s", action);
             }
         }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // keys bound for testing on glass and phone
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_MENU) {
+            if (bluetoothScannerService != null)
+                bluetoothScannerService.reconnect();
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
+
+    // NOTE: bindService/unbindService can be moved to onResume/onPause to disconnect from the scanner on app sleep
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_barcode_scanner);
         Log.start(this);
+
+        ((TextView) findViewById(R.id.barcodeScannerResults)).setMovementMethod(new ScrollingMovementMethod());
+
+        Log.debug("Binding from scanner service.");
+        bindService(new Intent(this, BluetoothBarcodeScannerService.class), bluetoothScannerConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        Log.debug("Pausing..");
-        this.unregisterReceiver(mReceiver);
-
-        timer.cancel();
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.debug("Unbinding from scanner service.");
+        unbindService(bluetoothScannerConnection);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.debug("Resuming..");
+        Log.debug("Resuming");
 
-        final BluetoothManager bluetoothManager = (BluetoothManager) this.getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
+        // register for scanner notifications
+        this.registerReceiver(receiver, new IntentFilter(BluetoothBarcodeScannerService.ACTION_BARCODE_SCANNED)); // barcode scanned
+        this.registerReceiver(receiver, new IntentFilter(BluetoothBarcodeScannerService.ACTION_SCANNER_CONNECTED)); // barcode-scanner connected
+        this.registerReceiver(receiver, new IntentFilter(BluetoothBarcodeScannerService.ACTION_SCANNER_DISCONNECTED)); // barcode-scanner disconnected
+    }
 
-        if (bluetoothAdapter.isDiscovering())
-            bluetoothAdapter.cancelDiscovery();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.debug("Pausing");
 
-        bluetoothAdapter.startDiscovery();
-
-        mReceiver = new BluetoothBroadcastReceiver();
-
-        this.registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-        this.registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED));
+        // unregister from scanner notifications
+        unregisterReceiver(receiver);
     }
 }
