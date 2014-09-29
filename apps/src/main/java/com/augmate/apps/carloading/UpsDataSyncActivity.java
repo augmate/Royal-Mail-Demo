5e8f0bbe6ed71dcf65fa5a8877e9503717287951
Johny Urgiles <johny@augmate.com>
2014-09-29 13:40:29 -0400

    Create bare activity for syncing ups data

---
+++ apps/src/main/java/com/augmate/apps/carloading/UpsDataSyncActivity.java
@@ -0,0 +1,13 @@
+package com.augmate.apps.carloading;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.augmate.apps.R;
+
+public class UpsDataSyncActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ups_data_sync_activity);
+    }
+}
