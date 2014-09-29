f6c952e4e3c81b27bf31bfca4c7a9b38a4851977
Johny Urgiles <johny@augmate.com>
2014-09-29 14:40:33 -0400

    Create connectivity checker to help show error messages.

--- apps/src/main/java/com/augmate/apps/carloading/UpsDataSyncActivity.java
+++ apps/src/main/java/com/augmate/apps/carloading/UpsDataSyncActivity.java
@@ -2,12 +2,26 @@ package com.augmate.apps.carloading;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import com.augmate.apps.R;
+import com.augmate.sdk.data.InternetChecker;
+import com.augmate.sdk.logger.Log;
 
 public class UpsDataSyncActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ups_data_sync_activity);
+
+        TextView view = (TextView) findViewById(R.id.internet_cnx_state);
+
+        if(new InternetChecker().isConnected(this)){
+            Log.info("Connected to the internet");
+            view.setText("Connected to the internet");
+        }else{
+            Log.info("Disconnected from the internet");
+            view.setText("Disconnected from the internet");
+        }
+
     }
 }
